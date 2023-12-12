7c291b06e1f1579f6554d1a645133a41e5f3b950
zeeshanahmad2311 <152972620+zeeshanahmad2311@users.noreply.github.com>
2023-12-12 23:11:32 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,68 @@
+gsap.from("nav",{
+    // y: -100,
+    duration: 2,
+    delay: 1,
+    opacity: 0,
+    
+    })
+    
+    gsap.from(".section1",{
+        y: -100,
+        duration: 2,
+        delay: 1,
+        opacity: -1,
+        
+        scrollTrigger: ".section1"
+    
+        })
+        
+    
+        gsap.from(".me ",{
+            y: 100,
+            duration: 3,
+            delay: 1,
+            opacity: 0,
+            
+            scrollTrigger: ".me"
+        
+            })
+
+            gsap.from(".main-about",{
+                y: 100,
+                duration: 3,
+                delay: 1,
+                opacity: 0,
+                
+                scrollTrigger: ".main-about"
+            
+                })
+
+                gsap.from(".main-skill",{
+                    y: 100,
+                    duration: 3,
+                    delay: 1,
+                    opacity: 0,
+                    
+                    scrollTrigger: ".main-skill"
+                
+                    })
+
+                    gsap.from(".main-project",{
+                        y: 100,
+                        duration: 3,
+                        delay: 1,
+                        opacity: 0,
+                        
+                        scrollTrigger: ".main-project"
+                    
+                        })
+
+                        gsap.from(".main-contect",{
+                            y: 100,
+                            duration: 3,
+                            delay: 1,
+                            opacity: 0,
+                            
+                            scrollTrigger: ".main-contect"
+                        
+                            })
